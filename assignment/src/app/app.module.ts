9598d2feb74b61a54cdd0fcdb16f0b89aa5fbfea
suot00 <truongkhacsuot661@gmail.com>
2019-10-16 21:26:21 +0700

    update

---
+++ assignment/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AssignmentComponent } from './assignment/assignment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AssignmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
